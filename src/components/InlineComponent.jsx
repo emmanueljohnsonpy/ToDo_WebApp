c153886ea73ee2e581b4c4a96152b1b1a56f3263
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-12-17 19:15:33 +0530

    first commit

---
+++ src/components/InlineComponent.jsx
@@ -0,0 +1,8 @@
+import styles from "./inlinecomponent.module.css";
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 className={styles.header}>Inline Component</h1>
+    </div>
+  );
+}
